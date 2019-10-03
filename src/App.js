1aa3a89beb0a53fac2a13bac8fe52519e591b223
Joshua Martinez <jam@Joshuas-MacBook-Pro.local>
2019-10-02 20:08:54 -0500

    new push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form.js';
+
+function App() {
+	return (
+		<div className="App">
+			<link rel="stylesheet" href="styles.css" />
+			<Form />
+		</div>
+	);
+}
+
+export default App;
